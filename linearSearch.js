408732c4a039134c3c87bd37c97364dde06cfdc2
Halsey Filbin <halsey.t.filbin@gmail.com>
2023-10-04 18:56:04 -0500

    linear search solution

--- linearSearch.js
+++ linearSearch.js
@@ -1,9 +1,26 @@
 function linearSearch(searchTerm, arr) {
-  return undefined;
+  for (let i = 0; i < arr.length; i++) {
+    let char = arr[i]
+
+    if (char === searchTerm) {
+      return i
+    }
+  }
 }
+console.log(linearSearch(0, [1,2,3,1]))
 
 function globalLinearSearch(searchTerm, arr) {
-  return [];
+  let charArr = []
+
+  for (let i = 0; i < arr.length; i++) {
+    let char = arr[i]
+
+    if (char === searchTerm) {
+      charArr.push(i)
+    }
+  }
+  return charArr
 }
+console.log(globalLinearSearch(0, [1, 2, 3, 1]))
 
 module.exports = { linearSearch, globalLinearSearch };
