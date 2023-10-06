76cf5984dc0f191b4f6fe332a61aa00313a67761
Halsey Filbin <halsey.t.filbin@gmail.com>
2023-10-05 19:23:42 -0500

    Comments on how code works

--- linearSearch.js
+++ linearSearch.js
@@ -2,24 +2,25 @@ function linearSearch(searchTerm, arr) {
   for (let i = 0; i < arr.length; i++) {
     let char = arr[i]
 
-    if (char === searchTerm) {
-      return i
+    if (char === searchTerm) { 
+      return i // will stop at first occurence and return index of first occurence of searchTerm
     }
   }
+  // will return undefined if searchTerm not in arr
 }
 console.log(linearSearch(0, [1,2,3,1]))
 
 function globalLinearSearch(searchTerm, arr) {
-  let charArr = []
+  let charArr = [] // initialize empty arr to store indices
 
   for (let i = 0; i < arr.length; i++) {
     let char = arr[i]
 
     if (char === searchTerm) {
-      charArr.push(i)
+      charArr.push(i) // push index where searchTerm occurs into new array
     }
   }
-  return charArr
+  return charArr // will return undefined if searchTerm not in arr
 }
 console.log(globalLinearSearch(0, [1, 2, 3, 1]))
 
